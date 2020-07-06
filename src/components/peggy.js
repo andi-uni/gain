828e8125e26aa25a5993c7845f46f0c37cc06ad5
Q3-113 <simonoswald@outlook.com>
2020-07-06 21:43:55 +0200

    updated peggy.js

--- src/components/peggy.js
+++ src/components/peggy.js
@@ -1,14 +1,13 @@
-const { initialize } = require("zokrates-js-node");
+import { initialize } from "zokrates-js";
+fs = require('fs-extra');
 
 function writeObject(filename,object) {
-  fs = require('fs');
   fs.writeFileSync(filename, JSON.stringify(object));
   console.log('Write to ...');
   console.log(filename);
 } 
 
 function readObject(filename) {
-  fs = require('fs')
   return JSON.parse(fs.readFileSync(filename, 'utf8'));
 }
 
