09787b979658ebecd93cd8b7d1b97dae4bea6b82
Q3-113 <simonoswald@outlook.com>
2020-07-06 23:04:21 +0200

    fixed peggy

--- src/components/peggy.js
+++ src/components/peggy.js
@@ -1,5 +1,5 @@
 import { initialize } from "zokrates-js";
-fs = require('fs-extra');
+let fs = require('fs-extra');
 
 function writeObject(filename,object) {
   fs.writeFileSync(filename, JSON.stringify(object));
@@ -21,15 +21,17 @@ function importResolver(location, path) {
 }
 
 initialize().then((zokratesProvider) => {
+
   // we have to initialize the wasm module before calling api functions
   const compileOutput = zokratesProvider.compile(
     "import \"hashes/sha256/512bitPacked\" as sha256packed\n \n def main(private field a, private field b, private field c, private field d) -> (field):\n field[2] h = sha256packed([a, b, c, d])\n h[0] == 263561599766550617289250058199814760685\n h[1] == 65303172752238645975888084098459749904\n return 1\n",
     "main",
     importResolver
   );
-  witness = zokratesProvider.computeWitness(compileOutput, ['0', '0', '0', '5']);
-  setup = readObject("setup.out");
-  provingKey = setup.pk;
-  proof = zokratesProvider.generateProof(compileOutput.program, witness.witness, provingKey) // proving Key is handed over from victor
+  let witness = zokratesProvider.computeWitness(compileOutput, ['0', '0', '0', '5']);
+  let setup = readObject("setup.out");
+  let provingKey = setup.pk;
+
+  let proof = zokratesProvider.generateProof(compileOutput.program, witness.witness, provingKey) // proving Key is handed over from victor
   writeObject("proof.out",proof);
 });
