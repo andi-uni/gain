7dfb29f61b92075ea97fcbeea6daa41bdb53feb9
Q3-113 <simonoswald@outlook.com>
2020-07-07 12:55:48 +0200

    how do I call verifyTX?

--- src/components/peggy.js
+++ src/components/peggy.js
@@ -1,5 +1,6 @@
 import { initialize } from "zokrates-js-node";
 let fs = require('fs-extra');
+let Web3 = require('web3');
 
 let VICTOR_ADR = '0xc31Eb6E317054A79bb5E442D686CB9b225670c1D'
 let PEGGY_ADR = '0x97026a8157f39101aefc4A81496C161a6b1Ce46A'
@@ -31,10 +32,51 @@ initialize().then((zokratesProvider) => {
     "main",
     importResolver
   );
-  let witness = zokratesProvider.computeWitness(compileOutput, ['0', '0', '0', '5']);
+
+  let witness;
+  try {
+    witness = readObject("witness.out");
+  } catch (error) {
+    witness = zokratesProvider.computeWitness(compileOutput, ['0', '0', '0', '5']);
+    writeObject("witness.out", witness);
+  }
+
   let setup = readObject("setup.out");
   let provingKey = setup.pk;
 
-  let proof = zokratesProvider.generateProof(compileOutput.program, witness.witness, provingKey) // proving Key is handed over from victor
-  writeObject("proof.out",proof);
+
+  let proof;
+  try {
+    proof = readObject("proof.out")
+  } catch (error) {
+    proof = zokratesProvider.generateProof(compileOutput.program, witness.witness, provingKey) // proving Key is handed over from victor
+    writeObject("proof.out",proof);
+  }
+  
+  let contractAddress = readObject('contractAddress.out');
+  let web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'), null, { transactionConfirmationBlocks: 1 /* This is the critical part */ });
+  let source = fs.readFileSync("Verifier.json");
+  let contract = JSON.parse(source);
+
+  let Contract = web3.eth.Contract(contract.abi, contractAddress);
+
+  let p = JSON.parse(proof).proof
+  console.log(contract.abi)
+  console.log(p)
+
+
+
+  let inputs = '0x0000000000000000000000000000000000000000000000000000000000000001';
+  // 'verifyTx(((p.a[0], p.a[1]),([p.b[0][0], p.b[0][1]],[p.b[1][0], p.b[1][1]]) (p.c[0]),p.c[1])),uint256[1])'
+
+  console.log(Contract.methods.verifyTx([p.a[0], p.a[1]], [[p.b[0][0], p.b[0][1]],[p.b[1][0], p.b[1][1]]], [p.c[0] ,p.c[1]],inputs).send({
+    from: PEGGY_ADR,
+    gas: 100000,
+    gasPrice: 0
+  }));
+  // for (let i in Contract.methods.contract) console.log(i)
+
+
+
+  
 });
